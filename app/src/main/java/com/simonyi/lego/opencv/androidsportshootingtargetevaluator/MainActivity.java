462f45ad819ee2637111e38206f7f7cb05448db1
izsoandras <izso.andras.zsolt@gmail.com>
2019-03-02 17:02:08 +0100

    Init commit

---
+++ app/src/main/java/com/simonyi/lego/opencv/androidsportshootingtargetevaluator/MainActivity.java
@@ -0,0 +1,22 @@
+package com.simonyi.lego.opencv.androidsportshootingtargetevaluator;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 100);
+        }
+    }
+}
